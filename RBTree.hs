345461665dcd7c416e1c683dc5a02196004f0a73
hkdnet <hkdnet@users.noreply.github.com>
2017-07-23 23:09:22 +0900

    lbalance, rbalanceに分割

--- RBTree.hs
+++ RBTree.hs
@@ -1,6 +1,7 @@
 module RBTree where
   data Color = R | B deriving Show
-  data RBTree a = Leaf | Node Color !(RBTree a) a !(RBTree a)
+  data RBTree a = Leaf | Node Color !(RBTree a) a !(RBTree a) deriving Show
+
 
   emptyRB :: RBTree a
   emptyRB = Leaf
@@ -17,16 +18,49 @@ module RBTree where
     where
       ins Leaf = Node R Leaf x Leaf
       ins ss@(Node color sa sy sb)
-        | x < y = balance color (ins sa) sy sb
-        | x > y = balance color sa sy (ins sb)
+        | x < sy = lbalance color (ins sa) sy sb
+        | x > sy = rbalance color sa sy (ins sb)
         | otherwise = ss
       Node _ a y b = ins s
 
-  balance :: Color -> RBTree a -> a -> RBTree a -> RBTree a
-  balance B (Node R (Node R a x b) y c) z d = Node R (Node B a x b) y (Node B c z d)
-  balance B (Node R a x (Node R b y c)) z d = Node R (Node B a x b) y (Node B c z d)
-  balance B a x (Node R (Node R b y c) z d) = Node R (Node B a x b) y (Node B c z d)
-  balance B a x (Node R b y (Node R c z d)) = Node R (Node B a x b) y (Node B c z d)
-  balance c l x r = Node c l x r
+  eq :: Eq a => RBTree a -> RBTree a -> Bool
+  eq (Node B xl x xr) (Node B yl y yr) = x == y && eq xl yl && eq xr yr
+  eq (Node R xl x xr) (Node R yl y yr) = x == y && eq xl yl && eq xr yr
+  eq _ _ = False
 
+  oldInsert :: Ord a => a -> RBTree a -> RBTree a
+  oldInsert x s = Node B a y b
+    where
+      ins Leaf = Node R Leaf x Leaf
+      ins ss@(Node color sa sy sb)
+        | x < sy = oldBalance color (ins sa) sy sb
+        | x > sy = oldBalance color sa sy (ins sb)
+        | otherwise = ss
+      Node _ a y b = ins s
+
+  fromOrdList :: Ord a => [a] -> RBTree a
+  fromOrdList list = fromOrdList' list emptyRB
+    where
+      fromOrdList' xs t = foldl (flip insert) t xs
+  oldFromOrdList :: Ord a => [a] -> RBTree a
+  oldFromOrdList list = fromOrdList' list emptyRB
+    where
+      fromOrdList' xs t = foldl (flip oldInsert) t xs
+
+  oldBalance :: Color -> RBTree a -> a -> RBTree a -> RBTree a
+  oldBalance B (Node R (Node R a x b) y c) z d = Node R (Node B a x b) y (Node B c z d)
+  oldBalance B (Node R a x (Node R b y c)) z d = Node R (Node B a x b) y (Node B c z d)
+  oldBalance B a x (Node R (Node R b y c) z d) = Node R (Node B a x b) y (Node B c z d)
+  oldBalance B a x (Node R b y (Node R c z d)) = Node R (Node B a x b) y (Node B c z d)
+  oldBalance c l x r = Node c l x r
+
+  lbalance :: Color -> RBTree a -> a -> RBTree a -> RBTree a
+  lbalance B (Node R (Node R a x b) y c) z d = Node R (Node B a x b) y (Node B c z d)
+  lbalance B (Node R a x (Node R b y c)) z d = Node R (Node B a x b) y (Node B c z d)
+  lbalance c l x r = Node c l x r
+
+  rbalance :: Color -> RBTree a -> a -> RBTree a -> RBTree a
+  rbalance B a x (Node R (Node R b y c) z d) = Node R (Node B a x b) y (Node B c z d)
+  rbalance B a x (Node R b y (Node R c z d)) = Node R (Node B a x b) y (Node B c z d)
+  rbalance c l x r = Node c l x r
 
--- RBTreeTest.hs
+++ RBTreeTest.hs
@@ -7,8 +7,11 @@ main =
 testRBTree :: IO()
 testRBTree
   | member 1 t = print "NG"
-  | not $ member 1 tt = print "NG"
+  | not $ member 1 t1 = print "NG"
   | otherwise = print "OK"
   where
     t = emptyRB
-    tt = insert 1 emptyRB
+    t1 = insert 1 emptyRB
+    t2 = fromOrdList [1, 2, 3, 4, 5, 6]
+
+
