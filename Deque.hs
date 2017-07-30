20a36f7870aa13c12025e4d0dec4830f93888cbf
hkdnet <hkdnet@users.noreply.github.com>
2017-07-31 00:05:57 +0900

    型をちょっと変更

--- Deque.hs
+++ Deque.hs
@@ -4,43 +4,43 @@ module Deque where
   -- 不変条件 要素が2つ以上あるとき、常にf, rのどちらも空でない
   -- 片方が空になったらもう片方を半分にして reverse する
   -- memo: 半分にするので長さをもっていたほうがよさそう
-  type Deque a = (Int, [a], Int, [a])
+  data Deque a = D Int [a] Int [a] deriving (Eq, Show)
 
 
   head :: Deque a -> a
-  head (0, _, 0, _) = error "empty"
-  head (0, _, 1, r) = GHC.List.head r
-  head (0, _, _, _) = error "制約違反"
-  head (_, f, _, _) = GHC.List.head f
+  head (D 0 _ 0 _) = error "empty"
+  head (D 0 _ 1 r) = GHC.List.head r
+  head (D 0 _ _ _) = error "制約違反"
+  head (D _ f _ _) = GHC.List.head f
 
   tail :: Deque a -> Deque a
-  tail (0, _, 0, _) = error "empty"
-  tail (0, _, 1, _) = (0, [], 0, [])
-  tail (0, _, _, _) = error "制約違反"
-  tail (1, _, rLen, r) = (nfl, nf, nrl, nr)
+  tail (D 0 _ 0 _) = error "empty"
+  tail (D 0 _ 1 _) = D 0 [] 0 []
+  tail (D 0 _ _ _) = error "制約違反"
+  tail (D 1 _ rLen r) = D nfl nf nrl nr
     where
       nfl = rLen `div` 2
       nrl = rLen - nfl
       nr = take nfl r
       nf = reverse $ drop nfl r
-  tail (lLen, _:xs, rLen, r) = (lLen - 1, xs, rLen, r)
-  tail (_, [], _, _) = error "制約違反"
+  tail (D lLen (_:xs) rLen r) = D (lLen - 1) xs rLen r
+  tail (D _ [] _ _) = error "制約違反"
 
   last :: Deque a -> a
-  last (0, _, 0, _) = error "empty"
-  last (1, r, 0, _) = GHC.List.last r
-  last (_, _, 0, _) = error "制約違反"
-  last (_, _, _, f) = GHC.List.head f
+  last (D 0 _ 0 _) = error "empty"
+  last (D 1 r 0 _) = GHC.List.last r
+  last (D _ _ 0 _) = error "制約違反"
+  last (D _ _ _ f) = GHC.List.head f
 
   init :: Deque a -> Deque a
-  init (0, _, 0, _) = error "empty"
-  init (1, _, 0, _) = (0, [], 0, [])
-  init (_, _, 0, _) = error "制約違反"
-  init (fLen, f, 1, _) = (nfl, nf, nrl, nr)
+  init (D 0 _ 0 _) = error "empty"
+  init (D 1 _ 0 _) = D 0 [] 0 []
+  init (D _ _ 0 _) = error "制約違反"
+  init (D fLen f 1 _) = D nfl nf nrl nr
     where
       nfl = fLen `div` 2
       nrl = fLen - nfl
       nf = take nfl f
       nr = reverse $ drop nfl f
-  init (fLen, f, rLen, _:xs) = (fLen, f, rLen - 1, xs)
-  init (_, _, _, [])  = error "制約違反"
+  init (D fLen f rLen (_:xs)) = D fLen f (rLen - 1) xs
+  init (D _ _ _ [])  = error "制約違反"
