1d10c78f043985d3eca0a748fd02f4ccd6408313
hkdnet <hkdnet@users.noreply.github.com>
2017-08-06 02:00:55 +0900

    バグってそうなケース追加

--- SplayHeapTest.hs
+++ SplayHeapTest.hs
@@ -5,6 +5,8 @@ main = do
   let h1 = insert 1 heap
 
   print h1
-  print $ foldl (flip insert ) heap [1..10]
+  print $ foldl (flip insert) heap [1..10]
+  print $ foldl (flip insert) heap $ reverse [1..10]
+  print $ foldl (flip insert) heap [1, 4, 5, 3, 2]
 
 
