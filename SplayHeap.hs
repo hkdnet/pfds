cfedf29b8f0943b2f5d25921a75ae995e9bbfeda
hkdnet <hkdnet@users.noreply.github.com>
2017-08-06 03:02:47 +0900

    コメント追加、場所移動

--- SplayHeap.hs
+++ SplayHeap.hs
@@ -4,6 +4,19 @@ module SplayHeap where
   insert :: Ord a => a -> SplayHeap a -> SplayHeap a
   insert x t = T (smaller x t) x (bigger x t)
 
+  bigger :: Ord a => a -> SplayHeap a -> SplayHeap a
+  bigger _ E = E
+  bigger pivot t@(T _ x b) =
+    if x <= pivot then bigger pivot b
+    else bigger' pivot t
+  bigger' :: Ord a => a -> SplayHeap a -> SplayHeap a
+  bigger' _ E = error "制約違反"
+  bigger' _ (T E x b) = T E x b
+  bigger' pivot (T (T a1 y a2) x b) =
+    if y <= pivot then T (bigger pivot a2) x b
+    else T (bigger pivot a1) y (T a2 x b)
+
+  -- smallerの非対称性がよくわからない
   smaller :: Ord a => a -> SplayHeap a -> SplayHeap a
   smaller _ E = E
   smaller pivot t@(T a x _) =
@@ -16,14 +29,3 @@ module SplayHeap where
     if y >= pivot then T a x (smaller pivot b1)
     else T (T a x b1) y (smaller pivot b2)
 
-  bigger :: Ord a => a -> SplayHeap a -> SplayHeap a
-  bigger _ E = E
-  bigger pivot t@(T _ x b) =
-    if x <= pivot then bigger pivot b
-    else bigger' pivot t
-  bigger' :: Ord a => a -> SplayHeap a -> SplayHeap a
-  bigger' _ E = error "制約違反"
-  bigger' _ (T E x b) = T E x b
-  bigger' pivot (T (T a1 y a2) x b) =
-    if y <= pivot then T (bigger pivot a2) x b
-    else T (bigger pivot a1) y (T a2 x b)
