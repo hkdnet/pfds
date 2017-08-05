efe74b598dbca42386a77ad2ff833c92f3819e4b
hkdnet <hkdnet@users.noreply.github.com>
2017-08-06 03:00:51 +0900

    smallerも実装

--- SplayHeap.hs
+++ SplayHeap.hs
@@ -6,9 +6,15 @@ module SplayHeap where
 
   smaller :: Ord a => a -> SplayHeap a -> SplayHeap a
   smaller _ E = E
-  smaller pivot (T a x b) =
+  smaller pivot t@(T a x _) =
     if x >= pivot then smaller pivot a
-    else T a x (smaller pivot b)
+    else smaller' pivot t
+  smaller' :: Ord a => a -> SplayHeap a -> SplayHeap a
+  smaller' _ E = error "制約違反"
+  smaller' _ (T a x E) = T a x E
+  smaller' pivot (T a x (T b1 y b2)) =
+    if y >= pivot then T a x (smaller pivot b1)
+    else T (T a x b1) y (smaller pivot b2)
 
   bigger :: Ord a => a -> SplayHeap a -> SplayHeap a
   bigger _ E = E
--- SplayHeapTest.hs
+++ SplayHeapTest.hs
@@ -13,8 +13,13 @@ main = do
   print $ foldl (flip insert) heap [1, 4, 5, 3]
   print $ foldl (flip insert) heap [1, 4, 5, 3, 2]
 
-  let expectedBigger = T (T (T (T E 1 E) 2 (T E 3 E)) 4 (T E 5 E)) 6 (T E 7 E)
+  let expectedBigger  = T (T (T (T E 1 E) 2 (T E 3 E)) 4 (T E 5 E)) 6 (T E 7 E)
+  let expectedSmaller = T (T E 1 E) 2 (T (T E 3 E) 4 (T (T E 5 E) 6 (T E 7 E)))
   if bigger 0 (T (T (T (T (T (T (T E 1 E) 2 E) 3 E) 4 E) 5 E) 6 E) 7 E) == expectedBigger
     then print "ok"
     else print $ bigger 0 (T (T (T (T (T (T (T E 1 E) 2 E) 3 E) 4 E) 5 E) 6 E) 7 E)
+
+  if smaller 8 (T E 1 (T E 2 (T E 3 (T E 4 (T E 5 (T E 6 (T E 7 E))))))) == expectedSmaller
+    then print "ok"
+    else print $ smaller 8 (T E 1 (T E 2 (T E 3 (T E 4 (T E 5 (T E 6 (T E 7 E))))))) 
 
