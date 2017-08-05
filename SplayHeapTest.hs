0557e894b34465a53a7cd22e226a9b69f7189a65
hkdnet <hkdnet@users.noreply.github.com>
2017-08-06 02:31:29 +0900

    biggerのバランス実装のケース追加

--- SplayHeapTest.hs
+++ SplayHeapTest.hs
@@ -13,4 +13,8 @@ main = do
   print $ foldl (flip insert) heap [1, 4, 5, 3]
   print $ foldl (flip insert) heap [1, 4, 5, 3, 2]
 
+  let expectedBigger = T (T (T (T E 1 E) 2 (T E 3 E)) 4 (T E 5 E)) 6 (T E 7 E)
+  if bigger 0 (T (T (T (T (T (T (T E 1 E) 2 E) 3 E) 4 E) 5 E) 6 E) 7 E) == expectedBigger
+    then print "ok"
+    else print $ (T (T (T (T (T (T (T E 1 E) 2 E) 3 E) 4 E) 5 E) 6 E) 7 E)
 
