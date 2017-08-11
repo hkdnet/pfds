1b0487522a0c1154fa80031d1209a4abdbb6d14a
hkdnet <hkdnet@users.noreply.github.com>
2017-08-11 21:13:44 +0900

    smaller が等しい要素を保持していることを確認

--- SplayHeapTest.hs
+++ SplayHeapTest.hs
@@ -13,6 +13,8 @@ main = do
   print $ foldl (flip insert) heap [1, 4, 5, 3]
   print $ foldl (flip insert) heap [1, 4, 5, 3, 2]
 
+  print "start test"
+
   let expectedBigger  = T (T (T (T E 1 E) 2 (T E 3 E)) 4 (T E 5 E)) 6 (T E 7 E)
   let expectedSmaller = T (T E 1 E) 2 (T (T E 3 E) 4 (T (T E 5 E) 6 (T E 7 E)))
   if bigger 0 (T (T (T (T (T (T (T E 1 E) 2 E) 3 E) 4 E) 5 E) 6 E) 7 E) == expectedBigger
@@ -23,3 +25,6 @@ main = do
     then print "ok"
     else print $ smaller 8 (T E 1 (T E 2 (T E 3 (T E 4 (T E 5 (T E 6 (T E 7 E))))))) 
 
+  if smaller 4 (T E 1 (T E 2 (T E 3 (T E 4 (T E 5 (T E 6 (T E 7 E))))))) == T (T E 1 E) 2 (T (T E 3 E) 4 E)
+    then print "ok"
+    else print $ smaller 4 (T E 1 (T E 2 (T E 3 (T E 4 (T E 5 (T E 6 (T E 7 E))))))) 
