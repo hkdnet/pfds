a9ddc26ec76471f8b184abb57bce647be5b8c7a5
hkdnet <hkdnet@users.noreply.github.com>
2017-09-23 16:37:52 +0900

    Update

--- BankersQueueTest2.hs
+++ BankersQueueTest2.hs
@@ -2,7 +2,7 @@ import BankersQueue
 import Control.Monad
 
 main = do
-  let limit = 100
+  let limit = 100000
   let xs = foldr BankersQueue.snoc' BankersQueue.empty [1..limit]
   let ys = foldr (\a -> \bs -> BankersQueue.tail' bs) xs [1..limit]
   print ys
