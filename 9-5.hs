3950d9f4356816100e9ccab7d4a05f63de041d50
hkdnet <satoko.itse@gmail.com>
2018-03-05 01:52:56 +0900

    test

--- 9-5.hs
+++ 9-5.hs
@@ -7,3 +7,21 @@ main = do
   print $ cons 1 list
   print $ cons 1 $ cons 1 list
   print $ cons 1 $ cons 1 $ cons 1 list
+  let list6 = cons 1 $ cons 2 $ cons 3 $ cons 4 $ cons 5 $ cons 6 list
+  print list6
+  print $ ZerolessRandomAccessList.lookup 0 list6
+  print $ ZerolessRandomAccessList.lookup 1 list6
+  print $ ZerolessRandomAccessList.lookup 2 list6
+  print $ ZerolessRandomAccessList.lookup 3 list6
+  print $ ZerolessRandomAccessList.lookup 4 list6
+  print $ ZerolessRandomAccessList.lookup 5 list6
+
+  let list7 = cons 1 $ cons 2 $ cons 3 $ cons 4 $ cons 5 $ cons 6 $ cons 7 list
+  print list7
+  print $ ZerolessRandomAccessList.lookup 0 list7
+  print $ ZerolessRandomAccessList.lookup 1 list7
+  print $ ZerolessRandomAccessList.lookup 2 list7
+  print $ ZerolessRandomAccessList.lookup 3 list7
+  print $ ZerolessRandomAccessList.lookup 4 list7
+  print $ ZerolessRandomAccessList.lookup 5 list7
+  print $ ZerolessRandomAccessList.lookup 6 list7
