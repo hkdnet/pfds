cfd8a7424cdd5a69ae81f920e0e88b770d097bbd
hkdnet <satoko.itse@gmail.com>
2018-03-04 22:47:00 +0900

    解けない >w<

--- 9-1.hs
+++ 9-1.hs
@@ -6,9 +6,14 @@ drop n (Zero:ts) = Zero:(Main.drop n ts)
 drop n (One t:ts) = drop' n (One t:ts)
   where
     drop' n (One t:ts)
+      | n == size t = Zero:ts
       | n > size t = Zero:(Main.drop (n - size t) ts)
       | otherwise  = (One (dropTree n t)):ts
-    dropTree n t = t
+
+dropTree :: Int -> Tree a -> Tree a
+dropTree n t = t
+  where
+    if n < w `div` 2 then Node w (updateTree i y t1) t2
 
 main =
   putStrLn "a"
