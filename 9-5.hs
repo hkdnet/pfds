82fcde534fd041bb4963fb1b46583adf8ba71baf
hkdnet <satoko.itse@gmail.com>
2018-03-05 01:46:08 +0900

    test

--- 9-5.hs
+++ 9-5.hs
@@ -2,4 +2,8 @@ import ZerolessRandomAccessList
 
 main :: IO()
 main = do
-  print "Hello"
+  let list = empty :: RList Int
+  print list
+  print $ cons 1 list
+  print $ cons 1 $ cons 1 list
+  print $ cons 1 $ cons 1 $ cons 1 list
