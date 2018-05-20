9be6118a01466a42257cd6d2adcc1f71695485b9
hkdnet <satoko.itse@gmail.com>
2018-05-20 15:05:46 +0900

    make

---
+++ 9-14.hs
@@ -0,0 +1,16 @@
+import HMSkewBinaryNumberRandomList
+
+
+main = do
+  let e0 = empty :: Queue Int
+  print e0
+  let e1 = snoc e0 1
+  let e2 = snoc e1 2
+  let e3 = snoc e2 3
+  let e4 = snoc e3 4
+  print e0
+  print e1
+  print e2
+  print e3
+  print $ HMSkewBinaryNumberRandomList.lookup 2 e3
+  print e4
--- makefile
+++ makefile
@@ -1,13 +1,16 @@
 all: \
 	bin/9-2 \
 	bin/9-4 \
-	bin/9-5
+	bin/9-5 \
+	bin/9-14
 
 bin/9-2: 9-2.hs
 	ghc -Wall -o $@ $^
 bin/9-4: 9-4.hs
 	ghc -Wall -o $@ $^
 bin/9-5: 9-5.hs
+	ghc -Wall -o $@ $^
+bin/9-14: 9-14.hs HMSkewBinaryNumberRandomList.hs SkewBinaryNumber.hs
 	ghc -Wall -o $@ $^
 
 .PHONY: all
