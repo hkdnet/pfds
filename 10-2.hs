bf28c1264865513bcd11777102e1d526ac582f8f
hkdnet <satoko.itse@gmail.com>
2018-06-30 19:01:13 +0900

    cons

--- 10-2.hs
+++ 10-2.hs
@@ -4,4 +4,14 @@ main :: IO()
 main = do
   -- 11 = 1011(2)
   let tree = One 0 (One (1, 2) (Zero (One (((3, 4), (5, 6)), ((7, 8), (9, 10))) Nil)))
+
+  let empty = Nil
+  let t1 = cons 1 empty
+  let t2 = cons 2 t1
+  let t3 = cons 3 t2
+  let t4 = cons 4 t3
   print tree
+  print t1
+  print t2
+  print t3
+  print t4
--- BootstrapSeq.hs
+++ BootstrapSeq.hs
@@ -1,2 +1,7 @@
 module BootstrapSeq where
   data Seq a = Nil | Zero (Seq(a, a)) | One a (Seq (a, a)) deriving Show
+
+  cons :: a -> Seq a -> Seq a
+  cons a Nil = One a Nil
+  cons a (Zero s) = One a s
+  cons a (One e s) = Zero (cons (a, e) s)
